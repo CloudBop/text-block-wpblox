079a730899b12dd7412f459f2aed4a393e7a8678
Cloudbop <boppincolin@gmail.com>
2024-02-18 21:19:22 +0000

    wp-create-block with added linting/format/prettier tools for WP+vscode

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '13d2c94aa59f5211f863');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '8f97a0ee9ef54893f2d9');
